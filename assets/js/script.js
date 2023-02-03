f74084fd13dc17ed05de590e99e7bc161d538a80
CcMong <fullstackcodex@gmail.com>
2023-02-03 11:50:26 +0000

    Created a function in script.js to render search history buttons.

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,29 @@ if(!parsedSearchHistory) {
     
 } else {userSearchHistory = parsedSearchHistory; // As long as parsedSearchHistory is not null, userSearchHistory will be set to what comes out of local storage
 }
+
+// Function for rendering the search history buttons
+
+function renderSearchHistoryButtons() {
+
+    $("#history").empty(); // Start by emptying the existing history on the page
+
+    
+    for (var i = 0; i < userSearchHistory.length; i++) { // Then loop through userSearchHistory array
+
+        if (i > 5) {
+
+            break; // Ensure that, according to the mock-up, only 6 cities appear in the search history
+
+        } else {
+
+            var newHistoryButton = $("<button>")
+            .text(userSearchHistory[i])
+            .attr("data-city", userSearchHistory[i])
+            .addClass("historyBtn btn btn-primary btn-sm col-12"); // ?????
+
+            $("#history").append(newHistoryButton); // We will place the newest city search at the beginning of the array. So we can append here to keep the latest search at the top of the search history stack         
+
+        }
+    };
+}
